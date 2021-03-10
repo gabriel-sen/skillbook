10f16d63f1cec1d6c255e30e1e671eff402ca658
gabriel sen <salut.gaby@gmail.com>
2021-03-10 12:45:04 +0100

    Stepper dans la vue de l'insecription / modification du champs Type de skills pour afficher des btn radios.

--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -9,8 +9,10 @@ use App\Entity\User;
 use App\Entity\Role;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,11 +22,40 @@ class RegisterFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('avatar')
-            ->add('firstname', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('password', PasswordType::class)
+            ->add('avatar', TextType::class, [
+                'label' => 'Votre image de profil :'
+            ])
+            ->add('firstname',TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Jean '
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Codeur '
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'email',
+                'attr' => [
+                    'placeholder' => 'Jean-codeur@campus-eni.fr'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe :',
+                'attr' => [
+                    'placeholder' => 'Jean@14324'
+                ]
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => 'Confirmez votre mot de passe',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Jean@14324'
+                ]
+            ])
             ->add('role', EntityType::class, [
                 'class' => Role::class,
                 'choice_label'=> 'role_name',
@@ -32,11 +63,14 @@ class RegisterFormType extends AbstractType
             ])
             ->add('skills', EntityType::class, [
                 'class' => Skills::class,
+                'label' => "Compétences",
                 'choice_label'=> 'skills_name',
-                'multiple' => true
+                'multiple' => true,
+                'expanded' => true
             ])
             ->add('bu', EntityType::class, [
                 'class' => BusinessUnit::class,
+                'label' => 'Unités de Business',
                 'choice_label'=> 'bu_name',
                 'multiple' => false
             ])
@@ -44,6 +78,9 @@ class RegisterFormType extends AbstractType
                 'class' => Project::class,
                 'choice_label'=> 'project_name',
                 'multiple' => false
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => "S'inscrire"
             ])
         ;
     }
